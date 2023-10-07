66af3f148777c21c64cbb26a5c13dad4bdde53ec
IVCodingStuff <isabellynascimento235@gmail.com>
2023-10-07 01:03:52 -0300

    .

--- CDD/desafio-array/ex-012.py
+++ CDD/desafio-array/ex-012.py
@@ -1,11 +1,21 @@
 media = 0
 maior = 0
 num = [0]*5
+pares =[0]*5
+e = 0
 for c in range(5):
     num[c] = int(input('Insira o {}º número: '.format(c+1)))
 for d in range(5):    
     if num[d] % 2 == 0:
-        print(num)
+        pares[e] = num[d]
+        e += 1
     if num[d] > maior:
         maior = num[d]
-        media += num[d]
+menor = maior
+for d in range(5):
+    if num[d] < menor:
+        menor = num[d]
+
+print('Números pares encontrados: {}'.format(pares))
+print('Maior número: {}'.format(maior))
+print('Menor número: {}'.format(menor))
