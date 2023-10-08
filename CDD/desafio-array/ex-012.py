912ccd9d305b98d2207ba8aa73db8e8ac64fa962
IV <60792969+IVCodingStuff@users.noreply.github.com>
2023-10-08 11:22:06 -0300

    Update ex-012.py

--- CDD/desafio-array/ex-012.py
+++ CDD/desafio-array/ex-012.py
@@ -1,18 +1,18 @@
 media = 0
 maior = 0
-num = [0]*5
-pares =[0]*5
+num = [0]*30
+pares =[0]*30
 e = 0
-for c in range(5):
+for c in range(30):
     num[c] = int(input('Insira o {}º número: '.format(c+1)))
-for d in range(5):    
+for d in range(30):    
     if num[d] % 2 == 0:
         pares[e] = num[d]
         e += 1
     if num[d] > maior:
         maior = num[d]
 menor = maior
-for d in range(5):
+for d in range(30):
     if num[d] < menor:
         menor = num[d]
 
