bdab88fed1b40e09507798368892f071682315b0
IV <60792969+IVCodingStuff@users.noreply.github.com>
2023-10-08 11:54:34 -0300

    Update ex-012.py

--- CDD/desafio-array/ex-012.py
+++ CDD/desafio-array/ex-012.py
@@ -2,7 +2,6 @@ media = 0
 maior = 0
 num = [0]*30
 pares =[0]*30
-maiormedia = 0
 e = 0
 for c in range(30):
     num[c] = int(input('Insira o {}º número: '.format(c+1)))
@@ -18,10 +17,7 @@ for d in range(30):
     if num[d] < menor:
         menor = num[d]
 media = media/30
-for f in range(30):
-  if num[f] >= media:
-    maiormedia += 1
 print('Números pares encontrados: {}'.format(pares))
 print('Maior número: {}'.format(maior))
 print('Menor número: {}'.format(menor))
-print('Números maiores que a média: {}'.format(maiormedia))
+print('Números maiores que a média: {}'.format(media))
