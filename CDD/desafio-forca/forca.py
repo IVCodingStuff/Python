c5302ded009e7a6bcbf2f967710f191e1e82cb15
IVCodingStuff <isabellynascimento235@gmail.com>
2023-10-15 21:26:52 -0300

    desafio-forca

--- CDD/desafio-forca/forca.py
+++ CDD/desafio-forca/forca.py
@@ -4,7 +4,12 @@ palavra = ('carisma', 'inteligencia', 'constituiçao', 'força', 'destreza', 'sa
 numpal = randint(0,5)
 palavra_secreta = list(palavra[numpal])
 resposta = list('_'*(len(palavra[numpal])))
-fim_jogo = False
-while fim_jogo == False:
-    jogo(palavra_secreta,resposta,tem, n_tem, fim_jogo)
-    
+n_tem = 1
+tem = 0
+fim_jogo = 0
+letras_digitadas = []
+while fim_jogo == 0:
+    resultados = jogo(palavra_secreta,resposta,tem, n_tem, fim_jogo, letras_digitadas)
+    n_tem = resultados[0]
+    fim_jogo = resultados[1]
+    letras_digitadas = resultados[2]
--- CDD/desafio-forca/lib.py
+++ CDD/desafio-forca/lib.py
@@ -1,31 +1,8 @@
 import os
-n_tem = 0
-tem = 0
-def jogo(palavra_secreta,resposta,tem, n_tem, fim_jogo):
+def jogo(palavra_secreta,resposta,tem, n_tem, fim_jogo, letras_digitadas):
     while True:
-        if resposta == palavra_secreta:
-            print('VOCÊ VENCEU!')
-            fim_jogo = True
-            break
-        elif n_tem == 0:
-            print ('____\n|   |\n|\n|\n|\n|')
-        elif n_tem == 1:
-            print ('____\n|   |\n| (>o<)\n|\n|\n|')
-        elif n_tem == 2:
-            print ('____\n|   |\n| (>o<)\n|   | \n|   \n|')
-        elif n_tem == 3:
-            print ('____\n|   |\n| (>o<)\n|  /| \n|   \n|')
-        elif n_tem == 4:
-            print ('____\n|   |\n| (>o<)\n|  /|\ \n|   \n|')
-        elif n_tem == 5:
-            print ('____\n|   |\n| (>o<)\n|  /|\ \n|   |\n| _/')
-        elif n_tem == 6:
-            print ('____\n|   |\n| (xox)\n|  /|\ \n|   |\n| _/_\_')
-            print('VOCÊ PERDEU!')
-            fim_jogo = True
-            break
-
         letra = input('Qual letra quer tentar? ').lower()
+        letras_digitadas.append(letra)
         print()
         for a in range(len(resposta)):
             if letra in palavra_secreta[a]:
@@ -33,9 +10,33 @@ def jogo(palavra_secreta,resposta,tem, n_tem, fim_jogo):
                 tem = 1
         if tem == 0:
             n_tem += 1
-        os.system('cls')
+        os.system('cls')  
+        print('Letras digitadas: {}'.format(letras_digitadas))
+        print()
         print(resposta)
         print()
-        print('Erros: ', n_tem)
+        
         tem = 0
-        return (fim_jogo)
+        if resposta == palavra_secreta:
+            print('VOCÊ VENCEU!')
+            fim_jogo = True
+            break
+        elif n_tem == 1:
+            print ('____\n|   |\n|\n|\n|\n|')
+        elif n_tem == 2:
+            print ('____\n|   |\n| (>o<)\n|\n|\n|')
+        elif n_tem == 3:
+            print ('____\n|   |\n| (>o<)\n|   | \n|   \n|')
+        elif n_tem == 4:
+            print ('____\n|   |\n| (>o<)\n|  /| \n|   \n|')
+        elif n_tem == 5:
+            print ('____\n|   |\n| (>o<)\n|  /|\ \n|   \n|')
+        elif n_tem == 6:
+            print ('____\n|   |\n| (>o<)\n|  /|\ \n|   |\n| _/')
+        elif n_tem == 7:
+            print ('____\n|   |\n| (xox)\n|  /|\ \n|   |\n| _/_\_')
+            print('VOCÊ PERDEU!')
+            fim_jogo = 1
+            break
+          
+    return [n_tem, fim_jogo, letras_digitadas]
