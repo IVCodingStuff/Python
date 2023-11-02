9b4380751cab72fbd75ffc0e222806a0c4139e90
IVCodingStuff <isabellynascimento235@gmail.com>
2023-11-02 17:37:24 -0300

    .

--- CDD/conta-bancaria/conta.py
+++ CDD/conta-bancaria/conta.py
@@ -7,8 +7,7 @@ print('===============')
 print ('   BANCO CDD')
 print('===============')
 while continuar:
-    if saldo == 0 and status == True:
-        print('\n1.Depositar\n2.Sacar\n3.Verificar o saldo\n4.Ativar conta\n5.Desativar conta\n6. Ativar limite\n7.Sair do sistema')
+        print('\n1.Depositar\n2.Sacar\n3.Verificar o saldo\n4.Ativar conta\n5.Desativar conta\n6.Ativar limite\n7.Sair do sistema')
         opcao = input('\nO que deseja fazer? ')
         if opcao in '1234567':
             if opcao == '1':
@@ -30,31 +29,6 @@ while continuar:
                 conta.ativar_limite()
 
             if opcao == '7':
-                continuar = conta.parar(continuar)
-        else:
-            print('Opção inválida.')
-            opcao = int(input('\nO que deseja fazer? '))
-    else:
-        print('\n1.Depositar\n2.Sacar\n3.Verificar o saldo\n4.Ativar conta\n5.Desativar conta\n6.Sair do sistema')
-        opcao = input('\nO que deseja fazer? ')
-        if opcao in '123456':
-
-            if opcao == '1':
-                saldo = conta.depositar()
-
-            if opcao == '2':
-                conta.sacar()
-
-            if opcao == '3':
-                conta.verificar_saldo()
-
-            if opcao == '4':
-                status = conta.ativar()
-                
-            if opcao == '5':
-                conta.desativar()
-
-            if opcao == '6':
                 continuar = conta.parar(continuar)
         else:
             print('Opção inválida.')
